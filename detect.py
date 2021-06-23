d64273c693707844ed8e8ada60dd71a777894871
Teddy <mickey23405383@gmail.com>
2021-06-24 02:05:22 +0800

    colab

--- detect.py
+++ detect.py
@@ -29,16 +29,11 @@ def collate_fn(x):
     return x[0]
 
 def detect(save_img=False, detector=None):
-    source, weights, view_img, imgsz = opt.source, opt.weights, opt.view_img,  opt.img_size
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://'))
+    source, weights,imgsz = 'input', 'weights.pt',640
 
-    # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    
     # Initialize
     #set_logging()
-    device = select_device(opt.device)
+    device = select_device('0')
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
@@ -47,22 +42,13 @@ def detect(save_img=False, detector=None):
     if half:
         model.half()  # to FP16
 
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = True
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=imgsz)
+    dataset = LoadImages(source, img_size=imgsz)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
-    t0 = time.time()
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
     for path, img, im0s, vid_cap in dataset:
@@ -85,17 +71,11 @@ def detect(save_img=False, detector=None):
 	        ]
         )
         # Process detections
-        print('!!!')
-        print(len(pred))
-        for i, det in enumerate(pred):  # detections per image
-            if webcam: 
-                p, s, im0, frame = Path(path[i]), '%g: ' % i, im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = Path(path), '', im0s, getattr(dataset, 'frame', 0)
+        for _, det in enumerate(pred):  # detections per image
+            _, s, im0, _ = Path(path), '', im0s, getattr(dataset, 'frame', 0)
 
-            save_path = str(save_dir / p.name)
             s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -107,9 +87,6 @@ def detect(save_img=False, detector=None):
 
                 # Write results
                 #faces = detector.mtcnn_detect(im0)
-                xyxy_list = []
-                label_list = []
-                DEBUG = 1
                 SPAND = 25
                 h, w, c = im0.shape
                 name = 'unknown'
@@ -125,73 +102,22 @@ def detect(save_img=False, detector=None):
                     if c2[0] + SPAND < w:
                         x_r  = c2[0] + SPAND
                     onestage = im0[y_u:y_d, x_l:x_r]
-                    #cv2.rectangle(im0, (x_l, y_u), (x_r, y_d), (0,0,255), 3, cv2.LINE_AA)
+
                     ten = transform1(onestage)
                     face_embedding = detector.resnet(ten.unsqueeze(0).to(device))
                     probs = [(face_embedding - detector.embeddings[i]).norm().item() for i in range(detector.embeddings.size()[0])] 
                     index = probs.index(min(probs))
-                    print(min(probs))
-                    #print(max(probs))
                     name = detector.names[index]
+                    print('')
                     print(name)
-                    if (save_img or view_img) and 1:  # Add bbox to image
-                        #faces = detector.mtcnn_detect(onestage)
-                        # if (faces is not None) and DEBUG:
-                        #     cv2.rectangle(im0, (x_l, y_u), (x_r, y_d), (0,0,255), 3, cv2.LINE_AA)
-                        #boxes, _ = detector.mtcnn_detect.detect(onestage)
-                        
-                        #cv2.imshow('TEST', onestage)
-                        #cv2.waitKey(0)
-                        """
-                        if (boxes is not None) and (faces is not None):
-                            for i,box in enumerate(boxes):
-                                face_embedding = detector.resnet(faces[i].unsqueeze(0).to(device))
-                                probs = [(face_embedding - detector.embeddings[i]).norm().item() for i in range(detector.embeddings.size()[0])] 
-                                index = probs.index(min(probs))
-                                print(min(probs))
-                                if min(probs) > TH:
-                                    name = 'unknown' 
-                                else:
-                                    name = detector.names[index]
-                        
-                        """
-                        #label = f'{names[int(cls)]} {conf:.2f}'
-                        label = f'{names[int(cls)]} {name}'
+                    
+                    label = f'{names[int(cls)]} {name}'
 
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)   
-                    #print(label_list)
-                    #print(xyxy_list)
-            # Print time (inference + NMS)
-                #cv2.imshow('DEBUG', im0)
-                #cv2.waitKey(0)
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=2)
+
             t2 = time_synchronized()
             print(f'{s}Done. ({t2 - t1:.3f}s)')
 
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
-                    raise StopIteration
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite('result.jpg', im0)
-                    print(f'Save! in {save_path}')
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
-
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter('result.mp4', cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
-
-    #print(f'Done. ({time.time() - t0:.3f}s)')
 
 class FaceNet():
     def __init__(self):
@@ -304,25 +230,6 @@ class FaceNetDetector():
         return cvimage
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    
     net = FaceNet()
     # init
     #net.gen_db('./face_db',True)
